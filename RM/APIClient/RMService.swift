9cb7d36b137516f4727e3a60a47172c148e9daae
Zoufishan Mehdi <zzmehdi@gmail.com>
2023-03-25 19:07:16 -0400

    Create API request

--- RM/APIClient/RMService.swift
+++ RM/APIClient/RMService.swift
@@ -13,7 +13,7 @@ final class RMService {
     
     private init() {}
     
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
         
     }
 }
--- RM/Controllers/Core/RMCharacterViewController.swift
+++ RM/Controllers/Core/RMCharacterViewController.swift
@@ -14,5 +14,16 @@ class RMCharacterViewController: UIViewController{
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(endpoint: .character,
+        queryParameters: [
+            URLQueryItem(name: "name", value: "rick"),
+            URLQueryItem(name: "status", value: "alive")
+        ])
+        print(request.url)
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+        
+        }
     }
 }
